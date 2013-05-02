9e8a2866324c73b681a23496d6bc0948ae601f02
Christian Lykke-Rasmussen <chrlykke89@gmail.com>
2013-05-02 11:02:25 +0200

    Adds performance test for range query on intervals without containment

--- C5.Tests/intervaled/Benchmarker.cs
+++ C5.Tests/intervaled/Benchmarker.cs
@@ -94,6 +94,29 @@ namespace C5.Tests.intervaled
 
             Console.WriteLine("Average creation time for {0} intervals: {1} ms", Intervaled.Count, (float)sw.ElapsedMilliseconds / Repetitions);
         }
+
+        [Test, TestCaseSource(typeof(WithoutContainment), "ConstructorCounts")]
+        public void RangeQuery(int count)
+        {
+            Intervaled = Factory(GenerateIntervals(count));
+
+            var length = 10;
+            var span = Intervaled.Span;
+            var step = (float)(span.High - span.Low - 20) / Repetitions;
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (var i = 0; i < Repetitions; i++)
+            {
+                var query = (int)(step * i);
+                // var query =; // random.Next(span.Low, span.High);
+                Intervaled.FindOverlaps(new IntervalBase<int>(query, query + length)).Count();
+            }
+            sw.Stop();
+
+            Console.WriteLine("Average creation time for {0} intervals: {1} ms", Intervaled.Count, (float)sw.ElapsedMilliseconds / Repetitions);
+        }
+
     }
 
     abstract class ContainmentOnly
