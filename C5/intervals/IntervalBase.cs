0d76726ac684997cf42ffcad95a44709934470b4
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-08-13 16:20:37 +0200

    Makes properties of IntervalBase readonly to avoid changes later on.

--- C5/intervals/IntervalBase.cs
+++ C5/intervals/IntervalBase.cs
@@ -7,10 +7,16 @@ namespace C5.intervals
     /// </summary>
     public class IntervalBase<T> : IInterval<T> where T : IComparable<T>
     {
-        public T Low { get; protected set; }
-        public T High { get; protected set; }
-        public bool LowIncluded { get; protected set; }
-        public bool HighIncluded { get; protected set; }
+        // Use read-only fields to avoid breaking data structures, if values were changed
+        private readonly T _low;
+        private readonly T _high;
+        private readonly bool _lowIncluded;
+        private readonly bool _highIncluded;
+
+        public T Low { get { return _low; } }
+        public T High { get { return _high; } }
+        public bool LowIncluded { get { return _lowIncluded; } }
+        public bool HighIncluded { get { return _highIncluded; } }
 
         /// <summary>
         /// Make a point with same Low and High and both endpoints included.
@@ -18,8 +24,8 @@ namespace C5.intervals
         /// <param name="query"></param>
         public IntervalBase(T query)
         {
-            Low = High = query;
-            LowIncluded = HighIncluded = true;
+            _low = _high = query;
+            _lowIncluded = _highIncluded = true;
         }
 
         /// <summary>
@@ -33,10 +39,10 @@ namespace C5.intervals
             if (low.CompareTo(high) >= 0)
                 throw new ArgumentException("Low must be smaller than high!");
 
-            Low = low;
-            High = high;
-            LowIncluded = true;
-            HighIncluded = false;
+            _low = low;
+            _high = high;
+            _lowIncluded = true;
+            _highIncluded = false;
         }
 
         /// <summary>
@@ -49,30 +55,33 @@ namespace C5.intervals
         /// <exception cref="ArgumentException">Thrown if interval is an empty point set</exception>
         public IntervalBase(T low, T high, bool lowIncluded, bool highIncluded)
         {
-            if (high.CompareTo(low) < 0 || (low.CompareTo(high) == 0 && !lowIncluded && !HighIncluded))
+            if (high.CompareTo(low) < 0 || (low.CompareTo(high) == 0 && !lowIncluded && !highIncluded))
                 throw new ArgumentException("Low must be smaller than high. If low and high are equal, both lowIncluded and highIncluded should be true!");
 
-            Low = low;
-            High = high;
-            LowIncluded = lowIncluded;
-            HighIncluded = highIncluded;
+            _low = low;
+            _high = high;
+            _lowIncluded = lowIncluded;
+            _highIncluded = highIncluded;
         }
 
         public IntervalBase(IInterval<T> i)
         {
-            Low = i.Low;
-            High = i.High;
-            LowIncluded = i.LowIncluded;
-            HighIncluded = i.HighIncluded;
+            _low = i.Low;
+            _high = i.High;
+            _lowIncluded = i.LowIncluded;
+            _highIncluded = i.HighIncluded;
         }
 
         public IntervalBase(IInterval<T> low, IInterval<T> high)
         {
-            Low = low.Low;
-            LowIncluded = low.LowIncluded;
+            if (low.CompareLowHigh(high) > 0)
+                throw new ArgumentException("Low must be smaller than high. If low and high are equal, both lowIncluded and highIncluded should be true!");
 
-            High = high.High;
-            HighIncluded = high.HighIncluded;
+            _low = low.Low;
+            _lowIncluded = low.LowIncluded;
+
+            _high = high.High;
+            _highIncluded = high.HighIncluded;
         }
 
         public override bool Equals(object that)
