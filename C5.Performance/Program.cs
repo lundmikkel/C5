a30a7eea3e6fccd8bd68790286c0f37a44ea1e56
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-09-02 10:51:50 +0200

    Adds performance console project to solution.

---
+++ C5.Performance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using C5.intervals;
+using C5.Tests.intervals;
+
+namespace C5.Performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ibs = new IntervalBinarySearchTree<int>(BenchmarkTestCases.DataSetB(10000));
+            Console.WriteLine(ibs.Span);
+            Console.ReadLine();
+        }
+    }
+}
