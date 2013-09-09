68ec88d293da32d4c41e57263a01d41dd52eb202
Anders Bech Mellson <anders@bechmellson.com>
2013-09-09 11:42:19 +0200

    Added NPerf and NPerfRunner to C5.Performance

---
+++ C5.Performance/NPerf/src/NPerf.Framework/PerfTearDownAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace NPerf.Framework
+{
+    using System;
+
+    /// <summary>
+    /// Defines a test tear-down method.
+    /// </summary>
+    /// <include file='NPerf.Framework.Doc.xml' path='doc/remarkss/remarks[@name="PerfTearDownAttribute"]'/>
+    /// <include file='NPerf.Framework.Doc.xml' path='doc/examples/example[@name="PerfSetUpTearDownAttribute"]'/>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class PerfTearDownAttribute : Attribute
+    {
+    }
+}
