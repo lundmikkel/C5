724cadab377966841feb6cb698aaea6d511d4577
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-09-11 10:21:39 +0200

    Adds NPerf test from http://www.codeproject.com/Articles/5945/NPerf-A-Performance-Benchmark-Framework-for-NET

---
+++ C5.Performance/DictionaryTester.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using NPerf.Framework;
+
+[PerfTester(typeof(IDictionary), 10)]
+public class DictionaryTester
+{
+    private int count;
+    private Random rnd = new Random();
+    [PerfRunDescriptor]
+    public double Count(int index)
+    {
+        return index * 1000;
+    }
+
+    [PerfSetUp]
+    public void SetUp(int index, IDictionary dic)
+    {
+        count = (int) Math.Floor(Count(index));
+    }
+
+    [PerfTest]
+    public void ItemAssign(IDictionary dic)
+    {
+        for (int i = 0; i < count; ++i)
+            dic[rnd.Next()] = null;
+    }
+}
