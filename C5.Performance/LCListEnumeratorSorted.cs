6d92d337185920b573ff40c8781cb4023f977a6d
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-09-17 10:54:53 +0200

    Add benchmarker for LCList's sorted enumerator

---
+++ C5.Performance/LCListEnumeratorSorted.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using C5.Tests.intervals;
+using C5.intervals;
+
+namespace C5.Performance
+{
+    class LCListEnumeratorSorted : Benchmarkable
+    {
+        private LayeredContainmentList2<int> _lclist;
+
+        protected override string BenchMarkName()
+        {
+            return "LCList Enumerable Sorted";
+        }
+
+        protected override void CollectionSetup()
+        {
+            _lclist = new LayeredContainmentList2<int>(BenchmarkTestCases.DataSetA(size));
+        }
+
+        protected override void Setup()
+        {
+        }
+
+        protected override double Call(int i)
+        {
+            var sum = 0.0;
+            foreach (var interval in _lclist.Sorted)
+                sum += interval.Low;
+            return sum;
+        }
+
+        protected override string Info()
+        {
+            return String.Format("{0,8:D}", size);
+        }
+    }
+}
--- C5.Performance/Program.cs
+++ C5.Performance/Program.cs
@@ -4,19 +4,26 @@ namespace C5.Performance
 {
     public class Program
     {
-        public static void Main(string[] args) {
-//            var jit = new JITCheater();
-//            jit.StartBenchmark();
-//            var simple = new SimpleBenchmark();
-//            simple.StartBenchmark(maxCollectionSize: 10000000);
+        public static void Main(string[] args)
+        {
+            var jit = new LCListEnumeratorSorted();
+            jit.StartBenchmark();
+            var simple = new SimpleBenchmark();
+            simple.StartBenchmark(100000);
 
-            var scalability = new SearchScalability();
-            scalability.StartBenchmark(maxCollectionSize: 10000000);
-            Console.Out.WriteLine("Done");
-            WaitForKey();
+            //var jit = new JITCheater();
+            //jit.StartBenchmark();
+            //var simple = new SimpleBenchmark();
+            //simple.StartBenchmark(maxCollectionSize: 10000000);
+
+            //var scalability = new SearchScalability();
+            //scalability.StartBenchmark(maxCollectionSize: 10000000);
+            //Console.Out.WriteLine("Done");
+            //WaitForKey();
         }
 
-        private static void WaitForKey() {
+        private static void WaitForKey()
+        {
             Console.ReadLine();
         }
     }
