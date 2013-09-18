cf0b80a0b4ef191f7ad136e710e07de4c2a02ca5
Anders Bech Mellson <anders@bechmellson.com>
2013-09-18 13:01:23 +0200

    Added C5.Performance.Wpf project

---
+++ C5.Performance.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace C5.Performance.Wpf {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            // Create the plotter with the benchmarks
+            var viewModel = new Plotter();
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
