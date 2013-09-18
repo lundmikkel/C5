28195fd736579af6eabd797e0f031891ae54da4f
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-09-18 14:31:13 +0200

    Adds two quick benchmarkers for IBS

---
+++ C5.Performance/IbsAddBenchmarker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using C5.intervals;
+
+namespace C5.Performance
+{
+    public class IbsAddBenchmarker : Benchmarkable
+    {
+        private IntervalBinarySearchTree<int> collection;
+        private IInterval<int>[] intervals;
+
+        protected override void CollectionSetup()
+        {
+            intervals = C5.Tests.intervals.BenchmarkTestCases.DataSetC(CollectionSize);
+            collection = new IntervalBinarySearchTree<int>();
+        }
+
+        protected override void Setup() { }
+
+        protected override double Call(int i)
+        {
+            foreach (var interval in intervals)
+                collection.Add(interval);
+
+            return collection.Count;
+        }
+
+        protected override string BenchMarkName()
+        {
+            return "IBS Add (AVL)";
+        }
+    }
+}
