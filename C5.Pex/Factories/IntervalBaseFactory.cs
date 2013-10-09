2df3b0215f14eb7d06b636dec7fb53e12a276b57
Anders Bech Mellson <anders@bechmellson.com>
2013-10-09 11:54:25 +0200

    Added Pex Test Project

---
+++ C5.Pex/Factories/IntervalBaseFactory.cs
@@ -0,0 +1,18 @@
+// <copyright file="IntervalBaseFactory.cs">Copyright ©  2013</copyright>
+
+using C5.intervals;
+using Microsoft.Pex.Framework;
+
+namespace C5.Pex.Factories
+{
+    /// <summary>A factory for C5.intervals.IntervalBase`1[System.Int32] instances</summary>
+    public static partial class IntervalBaseFactory
+    {
+        /// <summary>A factory for C5.intervals.IntervalBase`1[System.Int32] instances</summary>
+        [PexFactoryMethod(typeof(IntervalBase<int>))]
+        public static IntervalBase<int> Create(int low, int high, bool lowIncluded, bool highIncluded)
+        {
+            return new IntervalBase<int>(low,high,lowIncluded,highIncluded);
+        }
+    }
+}
