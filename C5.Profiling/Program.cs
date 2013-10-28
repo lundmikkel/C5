deab3e762d2d3265ec5e6629e625cb4082b48853
Anders Bech Mellson <anders@bechmellson.com>
2013-10-28 13:06:19 +0100

    Added C5.Profiling

---
+++ C5.Profiling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C5.Profiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Crasher uden Code Contracts!
+            var test = new C5.Tests.intervals.IntervalBinarySearchTreeAVL.RandomRemove();
+            test.SetUp();
+            test.AddAndRemove();
+        }
+    }
+}
