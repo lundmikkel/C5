f47cfc45c980941dc499beed0c27a646ba39b3b6
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-11-14 15:40:20 +0100

    Adds regions to IntervalCollectionTester to structure future test classes

---
+++ C5.Tests/intervals/IntervalCollectionTester.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using C5.intervals;
+using NUnit.Framework;
+
+namespace C5.Tests.intervals
+{
+    using Interval = IntervalBase<int>;
+
+    [TestFixture]
+    abstract class IntervalCollectionTester
+    {
+        #region Meta
+
+        protected abstract Type GetCollectionType();
+
+        /// <summary>
+        /// Override to add additional parameters to constructors.
+        /// </summary>
+        /// <returns>An object array of the extra parameters.</returns>
+        protected object[] AdditionalParameters()
+        {
+            return new object[0];
+        }
+
+        protected IIntervalCollection<IntervalBase<T>, T> Factory<T>(params IntervalBase<T>[] intervals) where T : IComparable<T>
+        {
+            var type = GetCollectionType();
+            Type[] typeArgs = { typeof(IntervalBase<T>), typeof(T) };
+            var genericType = type.MakeGenericType(typeArgs);
+
+            var additionalParameters = AdditionalParameters();
+            var parameters = new object[1 + additionalParameters.Length];
+            parameters[0] = intervals;
+            for (var i = 0; i < additionalParameters.Length; i++)
+                parameters[i + 1] = additionalParameters[i];
+
+            return (IIntervalCollection<IntervalBase<T>, T>) Activator.CreateInstance(genericType, parameters);
+        }
+
+        class IntervalBinarySearchTreeTester : IntervalCollectionTester
+        {
+            protected override Type GetCollectionType()
+            {
+                return typeof(IntervalBinarySearchTreeAvl<,>);
+            }
+        }
+
+        #endregion
+
+        #region Test Methods
+
+        [Test]
+        public void Test()
+        {
+            var collection = Factory(
+                new Interval(1, 2),
+                new Interval(2, 3)
+            );
+            Assert.AreEqual(collection.Count, 2);
+
+            var collection2 = Factory(new IntervalBase<bool>(true));
+            Assert.AreEqual(collection2.Count, 1);
+        }
+
+        #region Contructors
+        #endregion
+
+        #region Properties
+
+        #region Span
+
+        #endregion
+
+        #region Maximum Overlap
+        #endregion
+
+        #region Allows Reference Duplicates
+        #endregion
+
+        #endregion
+
+        #region Find Overlaps
+
+        #region Stabbing
+        #endregion
+
+        #region Range
+        #endregion
+
+        #endregion
+
+        #region Find Overlap
+
+        #region Stabbing
+        #endregion
+
+        #region Range
+        #endregion
+
+        #endregion
+
+        #region Count Overlaps
+
+        #region Stabbing
+        #endregion
+
+        #region Range
+        #endregion
+
+        #endregion
+
+        #region Extensible
+
+        #region Is Read Only
+        #endregion
+
+        #region Add
+
+        #region Events
+        #endregion
+
+        #endregion
+
+        #region Add All
+
+        #region Events
+        #endregion
+
+        #endregion
+
+        #region Remove
+
+        #region Events
+        #endregion
+
+        #endregion
+
+        #region Clear
+
+        #region Events
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #endregion
+    }
+
+    class DynamicIntervalTreeTesterReferenceDuplicatesFalse : IntervalCollectionTester
+    {
+        protected override Type GetCollectionType()
+        {
+            return typeof(DynamicIntervalTree<,>);
+        }
+
+        // DIT's standard behavior where we set the ReferenceDuplicates to false
+        protected new object[] AdditionalParameters()
+        {
+            return new object[] { false };
+        }
+    }
+
+    class DynamicIntervalTreeTesterReferenceDuplicatesTrue : IntervalCollectionTester
+    {
+        protected override Type GetCollectionType()
+        {
+            return typeof(DynamicIntervalTree<,>);
+        }
+
+        // DIT where we set the ReferenceDuplicates to true
+        protected new object[] AdditionalParameters()
+        {
+            return new object[] { true };
+        }
+    }
+
+    class IntervalBinarySearchTreeTester : IntervalCollectionTester
+    {
+        protected override Type GetCollectionType()
+        {
+            return typeof(IntervalBinarySearchTreeAvl<,>);
+        }
+    }
+}
