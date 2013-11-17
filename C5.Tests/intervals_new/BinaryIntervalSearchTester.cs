ac5ddfa734b8c0e09953d28a5fa8ef8ea584efff
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-11-17 15:02:37 +0100

    Adds IntervalBinarySearch data structure (BITS) for fast overlap counting

---
+++ C5.Tests/intervals_new/BinaryIntervalSearchTester.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using C5.intervals;
+
+namespace C5.Tests.intervals_new
+{
+    namespace BinaryIntervalSearch
+    {
+        #region Black-box
+
+        class BinaryIntervalSearchTester_BlackBox : IntervalCollectionTester
+        {
+            protected override Type GetCollectionType()
+            {
+                return typeof(BinaryIntervalSearch<,>);
+            }
+
+            protected override bool AllowsReferenceDuplicates()
+            {
+                return true;
+            }
+        }
+
+        #endregion
+    }
+}
