b471f18901a30b36bbc96e0e7cc6ca4fc24d6cf6
Mikkel Riise Lund <lund.mikkel@gmail.com>
2013-11-19 15:42:39 +0100

    Makes feed parser more friendly to general ics feed. Still needs some work though.

--- C5.UserExampleGoogleCalendar/IcsFeed.cs
+++ C5.UserExampleGoogleCalendar/IcsFeed.cs
@@ -12,7 +12,7 @@ namespace C5.UserExampleGoogleCalendar
         /// </summary>
         public static void Main(string[] args)
         {
-            const string url = "http://www.google.com/calendar/ical/bechmellson.com_eoauecnh84i50tbftksd5bfdl4%40group.calendar.google.com/public/basic.ics";
+            string url = "http://www.google.com/calendar/ical/bechmellson.com_eoauecnh84i50tbftksd5bfdl4%40group.calendar.google.com/public/basic.ics";
 
             // Parse url
             var events = ParseUrl(url);
@@ -67,23 +67,38 @@ namespace C5.UserExampleGoogleCalendar
             var events = new ArrayList<CalendarEvent>();
 
             // Example datestring to get the correct length for parsing
-            var dsLen = @"19700101T000000Z".Length;
-            var index = feed.IndexOf("DTSTART:", StringComparison.Ordinal);
+            var dateLength = @"19700101T000000Z".Length;
+            //var index = feed.IndexOf("DTSTART:", StringComparison.Ordinal);
+            var begin = 0;
+            var end = 0;
 
-            do
+            while ((begin = feed.IndexOf("BEGIN:VEVENT", end, StringComparison.Ordinal)) >= 0)
             {
-                var low = stringToDate(feed.Substring(index + "DTSTART:".Length, dsLen));
-                feed = feed.Substring(index + dsLen + "DTEND:".Length + "DTSTART:".Length + "\r\n".Length);
-                var high = stringToDate(feed.Substring(0, dsLen));
-                var titleIndex = feed.IndexOf("SUMMARY:", System.StringComparison.Ordinal);
-                var title = feed.Substring(titleIndex + "SUMMARY:".Length);
-                title = title.Substring(0, title.IndexOf("\r", System.StringComparison.Ordinal));
+                // Find end of event
+                end = feed.IndexOf("END:VEVENT", begin, StringComparison.Ordinal);
+
+                // Take a substring of the vevent found
+                var vevent = feed.Substring(begin, end - begin);
+
+                // Find title
+                var index = vevent.IndexOf("SUMMARY", StringComparison.Ordinal);
+                index = vevent.IndexOf(":", index, StringComparison.Ordinal) + 1;
+                // TODO: Is this the proper way to find the title?
+                var title = vevent.Substring(index, vevent.IndexOf("\r", index, StringComparison.Ordinal) - index);
+
+                // Find start time
+                index = vevent.IndexOf("DTSTART", StringComparison.Ordinal);
+                index = vevent.IndexOf(":", index, StringComparison.Ordinal) + 1;
+                var low = stringToDate(vevent.Substring(index, dateLength));
+
+                // Find end time
+                index = vevent.IndexOf("DTEND", StringComparison.Ordinal);
+                index = vevent.IndexOf(":", index, StringComparison.Ordinal) + 1;
+                var high = stringToDate(vevent.Substring(index, dateLength));
 
                 // Create and add calendar event to list
                 events.Add(new CalendarEvent(title, low, high));
-
-                index = feed.IndexOf("DTSTART:", StringComparison.Ordinal);
-            } while (index >= 0);
+            }
 
             // Sort events
             var eventArray = events.ToArray();
@@ -94,6 +109,7 @@ namespace C5.UserExampleGoogleCalendar
 
         private static DateTime stringToDate(String dateString)
         {
+            // TODO: Fix to parse the ICS date/date-time format
             var correctedDateString = dateString.Substring(0, dateString.Length - 3).Insert(4, "-").Insert(7, "-").Insert(13, ":");
             return DateTime.Parse(correctedDateString).AddHours(1);
         }
