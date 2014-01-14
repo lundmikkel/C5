452c8c30151e2cf4afebf50a79dc207430b03304
Mikkel Riise Lund <lund.mikkel@gmail.com>
2014-01-14 11:41:23 +1100

    Adds extension method to get interval type. Still in the early in its early fase.

---
+++ C5.Intervals.Tests/Extensions/IntervalTypeExtensionTester.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace C5.Intervals.Tests.Extensions
+{
+    [TestFixture]
+    class IntervalTypeExtensionTester
+    {
+        [Test]
+        public void Test()
+        {
+            var interval = new IntervalBase<int>(1, 2, IntervalType.Closed);
+
+            Assert.That(interval.IntervalType(), Is.EqualTo(IntervalType.Closed));
+        }
+
+        [Test]
+        public void CollectionTests()
+        {
+            var collection1 = new[]
+            {
+                new IntervalBase<int>(1),
+                new IntervalBase<int>(2),
+                new IntervalBase<int>(3),
+            };
+            var collection2 = new[]
+            {
+                new IntervalBase<int>(1),
+                new IntervalBase<int>(2),
+                new IntervalBase<int>(3),
+            };
+
+            Assert.That(collection1, Is.EqualTo(collection2));
+
+            CollectionAssert.AreEqual(collection1, collection2);
+        }
+    }
+}
---
+++ C5.Intervals/Extensions/IntervalTypeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace C5.Intervals
+{
+    public static class IntervalTypeExtensions
+    {
+        public static IntervalType IntervalType<T>(this IInterval<T> interval) where T : IComparable<T>
+        {
+            Contract.Requires(interval != null);
+            Contract.Ensures((Contract.Result<IntervalType>() == Intervals.IntervalType.Closed) == interval.IsClosed());
+            Contract.Ensures((Contract.Result<IntervalType>() == Intervals.IntervalType.Open) == interval.IsOpen());
+
+            return (IntervalType) ((interval.LowIncluded ? 2 : 0) + (interval.HighIncluded ? 1 : 0));
+        }
+
+        public static bool IsOpen<T>(this IInterval<T> interval) where T : IComparable<T>
+        {
+            Contract.Requires(interval != null);
+
+            return !interval.LowIncluded && !interval.HighIncluded;
+        }
+
+        public static bool IsClosed<T>(this IInterval<T> interval) where T : IComparable<T>
+        {
+            Contract.Requires(interval != null);
+
+            return interval.LowIncluded && interval.HighIncluded;
+        }
+    }
+}
