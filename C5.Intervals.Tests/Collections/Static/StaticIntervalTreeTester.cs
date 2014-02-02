0d8e5589282790305b427d07e327717c9fec4364
Mikkel Riise Lund <lund.mikkel@gmail.com>
2014-02-02 21:12:50 +1100

    Starts adding some tests for CountOverlaps for SIT

--- C5.Intervals.Tests/Collections/Static/StaticIntervalTreeTester.cs
+++ C5.Intervals.Tests/Collections/Static/StaticIntervalTreeTester.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace C5.Intervals.Tests
 {
+    using Interval = IntervalBase<int>;
+
     namespace StaticIntervalTree
     {
         #region Black-box
@@ -27,6 +30,119 @@ namespace C5.Intervals.Tests
         #endregion
 
         #region White-box
+
+        [TestFixture]
+        class StaticIntervalTreeTester_WhiteBox
+        {
+            private TestHelper TH;
+
+            #region SetUp
+
+            [SetUp]
+            public void SetUp()
+            {
+                TH = new TestHelper();
+            }
+
+            private static StaticIntervalTree<Interval, int> createSIT(Interval[] intervals = null)
+            {
+                if (intervals == null)
+                    intervals = new Interval[0];
+
+                return new StaticIntervalTree<Interval, int>(intervals);
+            }
+
+            private Interval[] A
+            {
+                get
+                {
+                    const int count = 7;
+                    var intervals = new Interval[count];
+
+                    for (var i = 0; i < count; i++)
+                        intervals[i] = new Interval(i * 2 + 1, i * 2 + 2, IntervalType.Open);
+
+                    return intervals;
+                }
+            }
+
+            public Interval[] B
+            {
+                get
+                {
+                    return new[]{
+                        new Interval(0, 10),
+                        new Interval(0, 5, true, false),
+                        new Interval(1, 5, true, true),
+                        new Interval(2, 5, true, false),
+                        new Interval(3, 5, true, true),
+                        new Interval(4, 5, true, false),
+                        new Interval(5, 5, true, true),
+                        new Interval(5, 6, false, true),
+                        new Interval(5, 7, true, true),
+                        new Interval(5, 8, false, true),
+                        new Interval(5, 9, true, true),
+                        new Interval(5, 10, false, true),
+                    };
+                }
+            }
+
+            #endregion
+
+            #region Count Overlaps
+
+            #region Stabbing
+
+            [Test]
+            public void CountOverlapsStabbing_WhileRootNotNull_Zero()
+            {
+                var collection = createSIT();
+                var query = TH.RandomInt;
+
+                Assert.That(collection.CountOverlaps(query), Is.EqualTo(0));
+            }
+
+            [Test]
+            public void CountOverlapsStabbing_CompareLessThanZero_Zero()
+            {
+                var collection = createSIT(A);
+
+                Assert.That(collection.CountOverlaps(0), Is.EqualTo(0));
+            }
+
+            [Test]
+            public void CountOverlapsStabbing_CompareGreaterThanZero_Zero()
+            {
+                var collection = createSIT(A);
+
+                Assert.That(collection.CountOverlaps(15), Is.EqualTo(0));
+            }
+
+            [Test]
+            public void CountOverlapsStabbing_CompareEqualsZero_Zero()
+            {
+                var collection = createSIT(A);
+
+                Assert.That(collection.CountOverlaps(7), Is.EqualTo(0));
+            }
+
+            // TODO: Finish tests
+            [Test]
+            public void CountOverlapsStabbing_()
+            {
+                var collection = createSIT(B);
+
+                Assert.That(collection.CountOverlaps(5), Is.EqualTo(6));
+            }
+
+            #endregion
+
+            #region Range
+            // TODO: Finish tests
+            #endregion
+
+            #endregion
+        }
 
         #region Legacy Tests
 
